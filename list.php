2cccca4ea1ee3b895177fa81886d82bf49ccbca3
vioharpama <42727462+vioharpama@users.noreply.github.com>
2018-10-05 22:04:05 +0700

    Update list.php

--- list.php
+++ list.php
@@ -1,30 +1,43 @@
 <?php
-require_once("db.php");git 
+require_once("db.php");
 ?>
-<table border=1>
-    <thead>
-        <th>Nama</th>
-        <th>Nim</th>
-        <th>Tanggal Lahir</th>
-    </thead>
-    <tbody>
-<?php
-$sql    = "SELECT * FROM siswa";
-$result = mysqli_query($conn, $sql);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>List Data</title>
+</head>
+<body>
+    <table width="400px" border=1>
+        <tr align="left">
+            <th>id</th>
+            <th>Nama</th>
+            <th>Nim</th>
+            <th>Tgl Lahir</th>
+        </tr>
 
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
-        echo "<td>" . $row["nama"]. "</td>"; 
-        echo "<td>" . $row["nim"]. "</td>";
-        echo "<td>" . $row["tgl_lahir"]. "</td>";
-        echo "</tr>";
-    }
-} else {
-    echo "0 results";
-}
-mysqli_close($conn);
-?> 
-    </tbody>
-</table>
+        <?php
+            $sql = "SELECT * FROM webdas";
+
+            $result = mysqli_query($connection, $sql);
+            $row = mysqli_num_rows($result);
+
+            if ($row > 0) {
+                while ($row = mysqli_fetch_array($result)) {
+                    $id = $row["id"];
+                    echo "<tr>";
+                    echo "<td>" . $row["nama"] . "</td>";
+                    echo "<td>" . $row["nim"] . "</td>";
+                    echo "<td>" . $row["tgl_lahir"] . "</td>";
+                    echo "</tr>";
+                }
+            }else {
+                echo "<tr><td colspan='4' align='center'>0 results.</td></tr>";
+            }
+        ?>
+
+    </table>
+</body>
+</html>

dc02d2b355b17f4737707b7a5f756be81042fe8d
vioharpama <42727462+vioharpama@users.noreply.github.com>
2018-10-05 22:05:18 +0700

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,20 +1,17 @@
 <?php
 require_once("db.php");
 
-$nama       = $_POST['nama'];
-$nim        = $_POST['nim'];
-$tgl_lahir  = $_POST['tgl_lahir'];
+$nama = $_POST["nama"];
+$nim = $_POST["nim"];
+$tgl_lahir = $_POST["tgl_lahir"];
 
-$sql = "INSERT INTO siswa(nama, nim, tgl_lahir) 
-        VALUES ('$nama','$nim','$tgl_lahir')";
+$sql = "INSERT INTO siswa(nama,nim,tgl_lahir) VALUES('$nama','$nim','$tgl_lahir')";
 
-
-if (mysqli_query($conn, $sql)) {
+if (mysqlI_query($connection, $sql)) {
     echo "New record created successfully<br>";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($connection);
 }
 
-mysqli_close($conn);
-echo "silahkan klik <a href='list.php'>link</a> untuk ke halaman list";
-?> 
+mysqli_close($connection);
+echo "Silahkan klik <a href='list.php'>link ini</a> untuk melanjutkan";

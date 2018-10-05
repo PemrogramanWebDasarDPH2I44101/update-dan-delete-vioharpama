856f09b34de3856038164c860986b86e6534f381
vioharpama <42727462+vioharpama@users.noreply.github.com>
2018-10-05 22:03:23 +0700

    Update form.php

--- form.php
+++ form.php
@@ -1,13 +1,30 @@
 <!DOCTYPE html>
-    <head>
-        <title></title>
-    </head>
-    <body>
-        <form action="submit.php" method="post">
-            Nama<input type="text" name="nama" id=""><br>
-            Nim<input type="text" name="nim" id=""><br>
-            Tanggal Lahir<input type="date" name="tgl_lahir" id=""><br>
-            <input type="submit" value="Kirim">
-        </form>
-    </body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Registrasi</title>
+</head>
+<body>
+    <form action="submit.php" method="post">
+        <table>
+            <tr>
+                <td>Nama</td>
+                <td><input type="text" name="nama"></td>
+            </tr>
+            <tr>
+                <td>Nim</td>
+                <td><input type="text" name="nim"></td>
+            </tr>
+            <tr>
+                <td>tgl_lahir</td>
+                <td><input type="date" name="tgl_lahir"></td>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="submit" value="Submit"></td>
+            </tr>
+        </table>
+    </form>
+</body>
 </html>
